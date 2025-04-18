1ae3a7f506f3758a41bc8eadd186074254efc6ab
Tianyue Qi <taylor_chyi@icloud.com>
2025-04-19 01:02:56 +0800

    - 初始化构建；

---
+++ src/main/java/com/ganlcy/glancy_backend/GlancyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ganlcy.glancy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlancyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlancyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganlcy/glancy_backend/GlancyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganlcy.glancy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlancyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
