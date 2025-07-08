b02f8063f902231d9b725e4d0fb7261f64e22e75
Tianyue Qi <taylor_chyi@icloud.com>
2025-07-09 01:24:11 +0800

    Return bound third-party account

--- src/main/java/com/glancy/backend/repository/ThirdPartyAccountRepository.java
+++ src/main/java/com/glancy/backend/repository/ThirdPartyAccountRepository.java
@@ -3,7 +3,9 @@ package com.glancy.backend.repository;
 import com.glancy.backend.entity.ThirdPartyAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface ThirdPartyAccountRepository extends JpaRepository<ThirdPartyAccount, Long> {
+    Optional<ThirdPartyAccount> findByProviderAndExternalId(String provider, String externalId);
 }
